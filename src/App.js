0e69f2d37ccdc9c0d7857634ea5e6eafb6f5297a
Nataly <nataly@192.168.1.13>
2023-02-28 13:58:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react'; 
 
 function App() {
+
+  const [ sovet, setSovet ] = useState("");
+  const [ chSovet, setChSovet ] = useState(0);
+
+  useEffect (()=> {
+    getSovet()},
+    [chSovet]);
+  
+
+  const getSovet = async() => {
+    const response = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await response.json();
+    setSovet(data.activity);
+  }
+ 
+  const changeSovet = () =>  {
+    setChSovet(chSovet+1);
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <p>{sovet}</p>
+      </div>
+      <div className='container'>
+        <button onClick={changeSovet}>what else to do</button>
+      </div>
     </div>
+   
   );
 }
 
